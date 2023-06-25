0ec2b4f4b45783e5514df4c95077fd449ba2f9a7
Evar-SF27 <evarfomuso29@gmail.com>
2023-06-25 22:11:55 +0100

    barChart created

--- js/layout.js
+++ js/layout.js
@@ -5,13 +5,13 @@ function layoutYear(data) {
 }
 
 function layoutSubject(data) {
-  let layoutData = data
+  let layoutData = data.filter(d => d.year == "2011-12")
 
   return layoutData
 }
 
 function layoutMean(data) {
-
+  
 }
 
 function layoutPercentage(data) {
